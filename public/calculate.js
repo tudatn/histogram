eca57c633fc6bf53d5e72a7e637c8dc1b79c3b26
Tu Dat Nguyen <you@example.com>
2017-09-29 18:53:16 -0700

    assign1.1

--- public/calculate.js
+++ public/calculate.js
@@ -1,98 +1,119 @@
 var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 
-var flag = true;
-
-function isValidBound(b) {
-    if (isNaN(b.value)) {
-        alert("Bounds must be a valid number!");
-        b.focus();
-        b.style.backgroundColor = 'red';
-        return 0.00;
-    }
-    else {
-        b.style.backgroundColor = 'white';
-        newValue = parseFloat(b.value);
-        // b.value = parseFloat(b.value).toFixed(2);
-        return newValue;
-    }
-}
-
-function getBound() {
+// get all bound objects
+function getCurrentBound() {
     var currentBound = [];
-    currentBound[0] = isValidBound(document.getElementById('bA+'));
-    currentBound[1] = isValidBound(document.getElementById('bA'));
-    currentBound[2] = isValidBound(document.getElementById('bA-'));
-    currentBound[3] = isValidBound(document.getElementById('bB+'));
-    currentBound[4] = isValidBound(document.getElementById('bB'));
-    currentBound[5] = isValidBound(document.getElementById('bB-'));
-    currentBound[6] = isValidBound(document.getElementById('bC+'));
-    currentBound[7] = isValidBound(document.getElementById('bC'));
-    currentBound[8] = isValidBound(document.getElementById('bC-'));
-    currentBound[9] = isValidBound(document.getElementById('bD'));
-    currentBound[10] = isValidBound(document.getElementById('bF'));
+    currentBound[0] = document.getElementById('bMax');
+    currentBound[1] = document.getElementById('bA+');
+    currentBound[2] = document.getElementById('bA');
+    currentBound[3] = document.getElementById('bA-');
+    currentBound[4] = document.getElementById('bB+');
+    currentBound[5] = document.getElementById('bB');
+    currentBound[6] = document.getElementById('bB-');
+    currentBound[7] = document.getElementById('bC+');
+    currentBound[8] = document.getElementById('bC');
+    currentBound[9] = document.getElementById('bC-');
+    currentBound[10] = document.getElementById('bD');
+    currentBound[11] = document.getElementById('bF');
     return currentBound;
 }
 
+// check all bound values are valid or not
+// raise alert if there is invalid value
+function isValidBoundValue(currentBound) {
+    var isValid = true;
+    for (var i = 0; i < 12; i++) {
+        if (isNaN(currentBound[i].value)) {
+            currentBound[i].focus();
+            currentBound[i].style.backgroundColor = 'red';
+            isValid = false;
+        }
+        else {
+            currentBound[i].style.backgroundColor = 'white';
+            currentBound[i].value = parseFloat(currentBound[i].value).toFixed(2);
+        }
+    }
+    if (isValid == false) {
+        alert("Bounds must be a valid number!");
+    }
+    return isValid;
+}
+
+// pre-condition: all bound values are valid
+// post-condition: return array of all bound values (float)
+function getBoundValue(currentBound) {
+    var boundValue = [];
+    for (var i = 0; i < 12; i++) {
+        boundValue[i] = parseFloat(currentBound[i].value);
+    }
+    return boundValue;
+}
+
+// pre-condition: all bound values are valid
+function isValidBoundRange(currentBound, currentBoundValue) {
+    var isValid = true;
+    max = currentBoundValue[0];
+    if (currentBoundValue[1] > max || currentBoundValue[1] <= currentBoundValue[2]) {
+        currentBound[1].focus();
+        currentBound[1].style.backgroundColor = 'red';
+        isValid = false;
+    }
+    for (var i = 2; i < 11; i++) {
+        if (currentBoundValue[i] <= currentBoundValue[i+1] || currentBoundValue[i] >= currentBoundValue[i-1]) {
+            currentBound[i].focus();
+            currentBound[i].style.backgroundColor = 'red';
+            isValid = false;
+        }
+    }
+    if (isValid == false) {
+        alert("Bounds range is overlapped!");
+    }
+    return isValid;
+}
+
 function recalculateHistogram() {
-    var currentBound = getBound();
+    var currentBound = getCurrentBound();
     var histogram = [0,0,0,0,0,0,0,0,0,0,0];
-    if (flag) {
-        for (var i = 0; i < grades.length; i++) {
-            if (grades[i] >= currentBound[0]) {
-                histogram[0]++;
-            }
-            else if (grades[i] >= currentBound[1]) {
-                histogram[1]++;
-            }
-            else if (grades[i] >= currentBound[2]) {
-                histogram[2]++;
-            }
-            else if (grades[i] >= currentBound[3]) {
-                histogram[3]++;
-            }
-            else if (grades[i] >= currentBound[4]) {
-                histogram[4]++;
-            }
-            else if (grades[i] >= currentBound[5]) {
-                histogram[5]++;
-            }
-            else if (grades[i] >= currentBound[6]) {
-                histogram[6]++;
-            }
-            else if (grades[i] >= currentBound[7]) {
-                histogram[7]++;
-            }
-            else if (grades[i] >= currentBound[8]) {
-                histogram[8]++;
-            }
-            else if (grades[i] >= currentBound[9]) {
-                histogram[9]++;
-            }
-            else {
-                histogram[10]++;
+    if (isValidBoundValue(currentBound)) {
+        var boundValue = getBoundValue(currentBound);
+        if (isValidBoundRange(currentBound, boundValue)) {
+            for (var i = 0; i < grades.length; i++) {
+                if (grades[i] >= boundValue[1]) histogram[0]++;
+                else if (grades[i] >= boundValue[2]) histogram[1]++;
+                else if (grades[i] >= boundValue[3]) histogram[2]++;
+                else if (grades[i] >= boundValue[4]) histogram[3]++;
+                else if (grades[i] >= boundValue[5]) histogram[4]++;
+                else if (grades[i] >= boundValue[6]) histogram[5]++;
+                else if (grades[i] >= boundValue[7]) histogram[6]++;
+                else if (grades[i] >= boundValue[8]) histogram[7]++;
+                else if (grades[i] >= boundValue[9]) histogram[8]++;
+                else if (grades[i] >= boundValue[10]) histogram[9]++;
+                else histogram[10]++;
             }
         }
     }
     return histogram;
 }
 
-function drawHistogram(obj, length) {
-    var width = (length/grades.length)*100;
+function drawHistogram(obj, length, scale) {
+    var width = (length/grades.length)*100*scale;
     obj.style.width = width + '%';
     obj.style.visibility = 'visible';
 }
 
 function update() {
     var histogram = recalculateHistogram();
-    drawHistogram(document.getElementById('A+'), histogram[0]);
-    drawHistogram(document.getElementById('A'), histogram[1]);
-    drawHistogram(document.getElementById('A-'), histogram[2]);
-    drawHistogram(document.getElementById('B+'), histogram[3]);
-    drawHistogram(document.getElementById('B'), histogram[4]);
-    drawHistogram(document.getElementById('B-'), histogram[5]);
-    drawHistogram(document.getElementById('C+'), histogram[6]);
-    drawHistogram(document.getElementById('C'), histogram[7]);
-    drawHistogram(document.getElementById('C-'), histogram[8]);
-    drawHistogram(document.getElementById('D'), histogram[9]);
-    drawHistogram(document.getElementById('F'), histogram[10]);
+    max = Math.max.apply(null, histogram);
+    scale = grades.length / max;
+    drawHistogram(document.getElementById('A+'), histogram[0], scale);
+    drawHistogram(document.getElementById('A'), histogram[1], scale);
+    drawHistogram(document.getElementById('A-'), histogram[2], scale);
+    drawHistogram(document.getElementById('B+'), histogram[3], scale);
+    drawHistogram(document.getElementById('B'), histogram[4], scale);
+    drawHistogram(document.getElementById('B-'), histogram[5], scale);
+    drawHistogram(document.getElementById('C+'), histogram[6], scale);
+    drawHistogram(document.getElementById('C'), histogram[7], scale);
+    drawHistogram(document.getElementById('C-'), histogram[8], scale);
+    drawHistogram(document.getElementById('D'), histogram[9], scale);
+    drawHistogram(document.getElementById('F'), histogram[10], scale);
 }
