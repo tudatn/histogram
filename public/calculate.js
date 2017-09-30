782fa164f8bdaf499f1a3e7d7c295ace11173ea7
Tu Dat Nguyen <you@example.com>
2017-09-29 19:51:17 -0700

    assign1.4 change % to no.

--- public/calculate.js
+++ public/calculate.js
@@ -1,4 +1,5 @@
-var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
+// var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
+var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0];
 
 // get all bound objects
 function getCurrentBound() {
@@ -98,8 +99,13 @@ function recalculateHistogram() {
 function drawHistogram(obj, length, scale) {
     var width = (length/grades.length)*100;
     obj.style.width = width*scale + '%';
-    obj.style.visibility = 'visible';
-    obj.textContent = "(" + width + "%)";
+    obj.textContent = "("+length+")";
+    if (length != 0) {
+        obj.style.visibility = 'visible';
+    }
+    else {
+        obj.style.visibility = 'hidden';
+    }
 }
 
 function update() {
